3cc0a82404ae65bc18bbf6c60d1bc7548f26fb5c
supavit-siriwan <supavit.siriwan@gmail.com>
2021-05-18 00:00:09 +0700

    Add custom track on Town3

--- run_agent.bat
+++ run_agent.bat
@@ -5,14 +5,15 @@ set DIR_ROOT=%CD%
 set CARLA_ROOT=%DIR_ROOT%\carla
 set SCENARIO_RUNNER_ROOT=%DIR_ROOT%\scenario_runner
 set LEADERBOARD_ROOT=%DIR_ROOT%\leaderboard
+set TRACKS_ROOT=%DIR_ROOT%\tracks
 
 set PYTHONPATH=%CARLA_ROOT%;%PYTHONPATH%
 set PYTHONPATH=%CARLA_ROOT%\dist\carla-0.9.11-py3.7-win-amd64.egg;%PYTHONPATH%
 set PYTHONPATH=%SCENARIO_RUNNER_ROOT%;%PYTHONPATH%
 set PYTHONPATH=%LEADERBOARD_ROOT%;%PYTHONPATH%
 
-set SCENARIOS="%LEADERBOARD_ROOT%\data\all_towns_traffic_scenarios_public.json"
-set ROUTES="%LEADERBOARD_ROOT%\data\routes_devtest.xml"
+set SCENARIOS="%TRACKS_ROOT%\Town03.json"
+set ROUTES="%TRACKS_ROOT%\Town03_test.xml"
 set REPETITIONS=1
 set DEBUG_CHALLENGE=1
 set TEAM_AGENT="%DIR_ROOT%\src\human_agent.py"
