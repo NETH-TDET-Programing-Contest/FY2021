d63c5a8183113408574ab9b47bdf9f5794095ba9
supavit-siriwan <supavit.siriwan@gmail.com>
2021-05-18 22:11:37 +0700

    Remove unnessary argument

--- run_agent.bat
+++ run_agent.bat
@@ -12,24 +12,11 @@ set PYTHONPATH=%CARLA_ROOT%\dist\carla-0.9.11-py3.7-win-amd64.egg;%PYTHONPATH%
 set PYTHONPATH=%SCENARIO_RUNNER_ROOT%;%PYTHONPATH%
 set PYTHONPATH=%LEADERBOARD_ROOT%;%PYTHONPATH%
 
-set SCENARIOS="%TRACKS_ROOT%\Town03.json"
-set ROUTES="%TRACKS_ROOT%\Town03_test.xml"
-set REPETITIONS=1
-set DEBUG_CHALLENGE=1
-set TEAM_AGENT="%DIR_ROOT%\src\human_agent.py"
-set CHECKPOINT_ENDPOINT="%LEADERBOARD_ROOT%\results.json"
-set CHALLENGE_TRACK_CODENAME=SENSORS
-
 .\venv\Scripts\python.exe "%LEADERBOARD_ROOT%\leaderboard\leaderboard_evaluator.py" ^
---scenarios=%SCENARIOS% ^
---routes=%ROUTES% ^
---repetitions=%REPETITIONS% ^
---track=%CHALLENGE_TRACK_CODENAME% ^
---checkpoint=%CHECKPOINT_ENDPOINT% ^
---agent=%TEAM_AGENT% ^
---agent-config=%TEAM_CONFIG% ^
---debug=%DEBUG_CHALLENGE% ^
---record=%RECORD_PATH% ^
---resume=%RESUME%
+--routes="%TRACKS_ROOT%\Town03.xml" ^
+--scenarios="%TRACKS_ROOT%\Town03.json" ^
+--agent="%DIR_ROOT%\src\human_agent.py" ^
+--agent-config="" ^
+--debug=1
 
 pause
